eed852ab0ab7b7b1a1e3e9ca624434a039b355b4
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2013-10-27 14:02:46 +0200

    Updated getsources script.

--- rpmbuild/getsources.sh
+++ rpmbuild/getsources.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if [ ! -f "./SOURCES/Python-2.7.5.tar.bz2" ]; then
-    wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tar.bz2 --no-check-certificate
+paths="http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tar.bz2
+http://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2
+https://pypi.python.org/packages/source/M/Mercurial/mercurial-2.7.2.tar.gz
+https://pypi.python.org/packages/source/s/setuptools/setuptools-1.1.6.tar.gz
+https://pypi.python.org/packages/source/s/supervisor/supervisor-3.0.tar.gz
+http://git-core.googlecode.com/files/git-1.8.4.1.tar.gz"
+
+if [ ! -d "SOURCES" ]; then
+    mkdir SOURCES
 fi
 
-if [ ! -f "./SOURCES/mercurial-2.7.2.tar.gz" ]; then
-    wget https://pypi.python.org/packages/source/M/Mercurial/mercurial-2.7.2.tar.gz --no-check-certificate
-fi
+for path in $paths
+do
+    filename=${path##*/}
+    if [ ! -f "./SOURCES/$filename" ]; then
+        echo "Downloading $filename"
+        wget -q $path --no-check-certificate -P SOURCES
+    fi
+done
 
-if [ ! -f "./SOURCES/Python-2.4.6.tar.bz2" ]; then
-    wget http://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2 --no-check-certificate
-fi
-
-if [ ! -f "./SOURCES/supervisor-3.0.tar.gz" ]; then
-    wget https://pypi.python.org/packages/source/s/supervisor/supervisor-3.0.tar.gz --no-check-certificate
-fi
-
-if [ ! -f "./SOURCES/git-1.8.4.1.tar.gz" ]; then
-    wget http://git-core.googlecode.com/files/git-1.8.4.1.tar.gz --no-check-certificate
-fi
